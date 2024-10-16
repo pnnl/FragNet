e2377d4b9ef98dab3958819130812bced2958142
pana982 <gihan.panapitiya@pnnl.gov>
2024-10-15 21:33:22 -0700

    rm sh

--- fragnet/exps/ft/pnnl_full/download.sh
+++
@@ -1,3 +0,0 @@
-scp marianas:/people/pana982/solubility/models/fragnet/fragnet/exps/ft/pnnl_full/fragnet_hpdl_exp1s_h4pt4_10/config_exp100.yaml fragnet_hpdl_exp1s_h4pt4_10/
-
-scp marianas:/people/pana982/solubility/models/fragnet/fragnet/exps/ft/pnnl_full/fragnet_hpdl_exp1s_h4pt4_10/ft_100.pt fragnet_hpdl_exp1s_h4pt4_10/
