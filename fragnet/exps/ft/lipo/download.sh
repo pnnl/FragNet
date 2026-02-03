89dcb492cdcab24f8a37cf6f1f924d7b3dfe4b4b
pana982 <pana982@example.com>
2026-02-02 21:33:11 -0800

    adding a notebook

--- fragnet/exps/ft/lipo/download.sh
+++
@@ -1,3 +0,0 @@
-scp marianas:/people/pana982/solubility/models/fragnet/fragnet/exps/ft/lipo/fragnet_hpdl_exp1s_pt4_30/ft_100.pt fragnet_hpdl_exp1s_pt4_30/
-
-scp marianas:/people/pana982/solubility/models/fragnet/fragnet/exps/ft/lipo/fragnet_hpdl_exp1s_pt4_30/config_exp100.yaml fragnet_hpdl_exp1s_pt4_30/
